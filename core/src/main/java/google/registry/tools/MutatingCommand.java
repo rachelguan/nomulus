67269fe44d39d337e7b8ff7bf485a5d9ffba8e91
rachelguan <rachelguan@google.com>
2021-04-22 20:18:01 +0000

    remove unnessary formatting for existing code

--- core/src/main/java/google/registry/tools/MutatingCommand.java
+++ core/src/main/java/google/registry/tools/MutatingCommand.java
@@ -127,7 +127,7 @@ public abstract class MutatingCommand extends ConfirmingCommand implements Comma
       String changeText;
       if (type == ChangeType.UPDATE) {
         String diffText = prettyPrintEntityDeepDiff(
-          oldEntity.toDiffableFieldMap(), newEntity.toDiffableFieldMap());
+            oldEntity.toDiffableFieldMap(), newEntity.toDiffableFieldMap());
         changeText = Optional.ofNullable(emptyToNull(diffText)).orElse("[no changes]\n");
       } else {
         changeText = MoreObjects.firstNonNull(oldEntity, newEntity) + "\n";
